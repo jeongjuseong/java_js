e6ad2c304a689ac0edd4ac77c5e4bb2a087792c5
jeongjuseong <134986443+jeongjuseong@users.noreply.github.com>
2023-05-31 20:59:09 +0900

    과제중

--- java/src/day2/homework/Ex1.java
+++ java/src/day2/homework/Ex1.java
@@ -3,7 +3,14 @@ package day2.homework;
 public class Ex1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//콘솔에서 정수 2개의 문자 (산술연산자)를 입력받아 출력하는 코드를 작성하시오.
+		//예시 : 두 정수의 산술연산자를 입력하세요 ex:  1 + 2
+		int num1 = 1, num2 = 2;
+		System.out.println(num1 + "+" + num2);
+		System.out.println(num1 + "-" + num2);
+		System.out.println(num1 + "*" + num2);
+		System.out.println(num1 + "/" + num2);
+		System.out.println(num1 + "%" + num2);
 
 	}
 
--- java/src/day2/homework/Ex2.java
+++ java/src/day2/homework/Ex2.java
@@ -3,8 +3,10 @@ package day2.homework;
 public class Ex2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		/*두 정수를 입력 받아 두 정수의 산술 연산 결과를 출력하는 코드를 작성하세요.
+		 *단 2번째 숫자는 0이 아니여야함.ex) 1 2 (엔터) 
+		 */
+		System.out.println("두 정수와 
 	}
 
 }
