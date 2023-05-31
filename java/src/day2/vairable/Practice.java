93b32280a9b0b7beb1b402c835006c1405f96b17
jeongjuseong <134986443+jeongjuseong@users.noreply.github.com>
2023-05-31 12:41:33 +0900

    자바 변수연습문제

---
+++ java/src/day2/vairable/Practice.java
@@ -0,0 +1,18 @@
+package day2.vairable;
+
+public class Practice {
+
+	public static void main(String[] args) {
+		
+
+		//문장은 ''로 감싸줘야한다. ""가 아님.
+		char ch1 = 'A';
+		/*국어, 영어 수학 성적을 입력하여 평균을 구하려 한다. 이 때 필요한 변수들을 선언하세요.
+		 * */
+		int kor, eng, math;
+		double avg;
+		
+
+	}
+
+}
