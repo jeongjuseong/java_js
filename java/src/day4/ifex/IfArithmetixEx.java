d1db84cc0d39bb81070e1eafd52b0a5131bb1ab2
jeongjuseong <134986443+jeongjuseong@users.noreply.github.com>
2023-06-02 14:38:39 +0900

    자바 if else if 산술연산 예제

---
+++ java/src/day4/ifex/IfArithmetixEx.java
@@ -0,0 +1,47 @@
+package day4.ifex;
+
+import java.util.*;
+
+public class IfArithmetixEx {
+
+	public static void main(String[] args) {
+		//산술 연산자와 두 정수를 입력받아 산술연산자에 맞는 연산결과를
+		//출력하는 코드를 작성하세요
+		//산술연산자 : op
+		//op가 + 이면 num1 + num2 를 출력하고
+		//op가 - 이면 num1 - num2 를 출력하고
+		//op가 * 이면 num1 * num2 를 출력하고
+		//op가 / 이면 num1 / num2 를 출력하고
+		//op가 % 이면 num1 % num2 를 출력하고
+		//op가 산술연산자가 아니면 is not arithmetic operator!를 출력로 출력
+		
+		int num1, num2;
+		char op;
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("input 2 nums and arithmetic operator(ex:1+2) : ");
+		num1 = sc.nextInt();
+		op = sc.next().charAt(0);
+		num2 = sc.nextInt();
+		
+
+		
+		if(op == '+') {
+			System.out.println(num1 + " " + op + " " + num2 + " = " + (num1+num2));
+		}else if (op == '-') {
+			System.out.println(num1 + " " + op + " " + num2 + " = " + (num1-num2));
+		}else if (op == '*') {
+			System.out.println(num1 + " " + op + " " + num2 + " = " + (num1*num2));
+		}else if (op == '/') {
+			System.out.println(num1 + " " + op + " " + num2 + " = " + ((double)num1/num2));
+		}else if (op == '%') {
+			System.out.println(num1 + " " + op + " " + num2 + " = " + (num1%num2));
+		}else { 
+			System.out.println(op + " is not arithmetic op! ");
+		}
+		
+		
+		sc.close();
+	}
+
+}
