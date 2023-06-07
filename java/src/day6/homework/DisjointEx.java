284f34a332dab147968993870433ae223a7cc827
jeongjuseong <134986443+jeongjuseong@users.noreply.github.com>
2023-06-07 14:47:53 +0900

    자바 6일차 숙제

---
+++ java/src/day6/homework/DisjointEx.java
@@ -0,0 +1,14 @@
+package day6.homework;
+
+import java.util.*;
+
+public class DisjointEx {
+
+	public static void main(String[] args) {
+		//두 정수를 입력받아 두 정수가 서로소 관계인지 판별하는 코드를 작성
+		//서로소 최대 공약수가 1인 두수의 관계
+		
+
+	}
+
+}
