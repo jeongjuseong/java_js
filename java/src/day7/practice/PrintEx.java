f40ecd918d1c57e59b40b7354c01cf0aae7c510a
jeongjuseong <134986443+jeongjuseong@users.noreply.github.com>
2023-06-08 14:43:50 +0900

    자바 콘솔 출력 예제

---
+++ java/src/day7/practice/PrintEx.java
@@ -0,0 +1,22 @@
+package day7.practice;
+
+public class PrintEx {
+
+	public static void main(String[] args) {
+		//원하는 값을 출력한 후 엔터까지 입력
+		System.out.println("abc");
+		System.out.println('a');	
+		System.out.println(1);
+		//print() : 원하는 값을 출력
+		System.out.print("abc");
+		System.out.print('a');	
+		System.out.print(1);
+		System.out.println();
+		//printf() : 원하는 값을 출력. 서식문자를 이용
+		System.out.printf("string : %s/n", "abc");
+		System.out.printf("integer : %d/n", 1);
+		System.out.printf("char : %c/n", 'a');
+	}
+
+	
+}
