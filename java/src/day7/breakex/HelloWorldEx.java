2bd1bd546fb27d23e91b1d13f8ea40783e5782a8
jeongjuseong <134986443+jeongjuseong@users.noreply.github.com>
2023-06-08 14:42:46 +0900

    자바 break 예제

---
+++ java/src/day7/breakex/HelloWorldEx.java
@@ -0,0 +1,16 @@
+package day7.breakex;
+
+public class HelloWorldEx {
+
+	public static void main(String[] args) {
+		int i = 1;
+		while(true) {
+			System.out.println("Hello world!");
+			if(i==5) {
+				break;
+			}i++;
+		}
+
+	}
+
+}
