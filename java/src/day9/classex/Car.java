8dc8f33a492e7eff30f595d5679da3ddbda174f3
jeongjuseong <134986443+jeongjuseong@users.noreply.github.com>
2023-06-12 14:34:22 +0900

    자바 9일차 자동차 클래스 예제

---
+++ java/src/day9/classex/Car.java
@@ -0,0 +1,81 @@
+package day9.classex;
+
+public class Car {
+	
+	//멤버 변수 (필드)
+	private String name;
+	private String company;
+	private int year;
+	private int speed;
+	private char gear;
+	private boolean power;
+	
+	//생성자
+	public Car(String _name, String _company, int _year) {
+		name = _name;
+		company = _company;
+		year = _year;
+		gear = 'P';
+	}
+	
+	//메서드 
+	/**자동차 정보를 출력하는 메서드
+	 * 매개변수 : 없음 -> 
+	 * 리턴타입 : 없음 -> void
+	 * 메서드명 : printInfo
+	 */
+	public void printInfo() {
+		System.out.println("자동차 조회");
+		System.out.println("제조사 : " + company);
+		System.out.println("모델 : " + name);
+		System.out.println("연식 : " + year);
+		System.out.println("전원 : " + (power?"On":"off"));
+		System.out.println("기어 : " + gear);
+		System.out.println("속도 : " + speed);
+		
+	}
+	/**속력이 1 증가/1 감소 하는 메서드
+	 * 매개변수 : 증가할건지 -> boolean
+	 * 리턴타입 : 없음
+	 * 메서드명 : speedChange
+	 */
+	public void speedChange(boolean positive) {
+		//전원이 꺼져있으면 종료
+		if(!power) {
+			return;
+		}
+		//속력이 증가하는 경우이면
+		if(gear == 'N' || gear == 'P') {
+			return;
+		}
+		if(positive) {
+			speed += 1;
+		}
+		//속력이 감소하는 경우이고 속력이 0보다 크면
+		else if(speed > 0){
+			speed -= 1;
+		}
+		
+		
+	}
+	/**자동차 시동을 켜거나 끄는 메서드
+	 * 매개변수 : 없음 
+	 * 리턴타입 : void
+	 * 메서드명 : turn
+	 */
+	public void turn() {
+		if(!power) {
+			power = true;
+		}else if(gear == 'P') {
+			power = false;
+		}
+	}
+
+	public void setGear(char _gear) {
+		gear = _gear;
+	}
+	
+}
+
+
+
