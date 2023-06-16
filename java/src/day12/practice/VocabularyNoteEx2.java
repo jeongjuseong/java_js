950243b6658bdbaf5a20867b4e62fad4a30afbc6
jeongjuseong <134986443+jeongjuseong@users.noreply.github.com>
2023-06-16 12:36:16 +0900

    자바 :  단어장 예제

--- java/src/day12/practice/VocabularyNoteEx2.java
+++ java/src/day12/practice/VocabularyNoteEx2.java
@@ -7,9 +7,35 @@ public class VocabularyNoteEx2 {
 				new Word("abc"),
 				new Word("def","def is define")
 		};
-
+		
 		VocabularyNote note = new VocabularyNote(wordList);
+		//note.print();
+		
+		Word word = new Word("a", "a");
+		note.insert(word);
+		//note.print();
+		note.insert("b","b is b");
+		//note.print();
+		
+		//note.delete("c");
+		note.delete("a");
+		//note.print();
+		
+		//note.search("a");
+		//note.search("b");
+		
+		note.insert("b", "b");
+		//note.print();
+		
+		note.updateMening("b", 2, "B");
+		note.updateMening("b", 3, "B"); //Wrong number
+		//note.print();
+		
+		note.updateTitle("b", "B");
+		//No words found 출력. 이미 앞에서 수정해서 b가 없음
+		note.updateTitle("b", "B");
 		note.print();
+		
 	}
 
 }
