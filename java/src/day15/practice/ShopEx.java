fce4f64844c61fb24852ed13210df0f254e53e87
jeongjuseong <134986443+jeongjuseong@users.noreply.github.com>
2023-06-20 14:55:30 +0900

    자바 : 제품 판매 프로그램 예제

---
+++ java/src/day15/practice/ShopEx.java
@@ -0,0 +1,26 @@
+package day15.practice;
+
+import java.util.Scanner;
+
+public class ShopEx {
+
+	public static void main(String[] args) {
+		//가전제품(tv, 라디오, 에어컨)을 관리하는 프로그램을 작성
+		/**메뉴
+		 * 1. 제품판매: 어떤고객이 어떤제품을 구매했는지 관리
+		 * 2. 제품입고: 새 제품이 추가됨. 어떤 제품이 몇개 추가됐는지 관리
+		 * 3. 제품조회: 제품 정보 조회, 제품명으로 제품을 조회
+		 * 4. 매출조회: 판매된 제품 매출 조회. 누적 매출
+		 * 5. 고객등록: 고객을 추가하여 고객을 관리
+		 * 6. 프로그램 종류
+		 */
+	
+		
+	}
+
+
+}
+
+class Shop{
+	
+}
