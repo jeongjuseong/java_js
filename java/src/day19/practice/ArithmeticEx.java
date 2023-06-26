54221487d322e361a76b5ea8a89436513d9fff8c
jeongjuseong <134986443+jeongjuseong@users.noreply.github.com>
2023-06-26 14:49:19 +0900

    자바 : 예외처리 예제

---
+++ java/src/day19/practice/ArithmeticEx.java
@@ -0,0 +1,71 @@
+package day19.practice;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class ArithmeticEx {
+
+	public static void main(String[] args) {
+		//두 정수와 산술연산자를 입력했을때 연산 결과를 출력하는 코드를 작성
+		//예외 처리도 추가
+		Scanner sc = new Scanner(System.in);
+		System.out.println("input (ex:1 + 2) : ");
+		try {
+		int num1 = sc.nextInt();
+		char op = sc.next().charAt(0);
+		int num2 = sc.nextInt();
+			
+		//연산자에 따른 결과를 출력
+		switch(op) {
+			case '+':
+				System.out.println("" + num1 + op + num2 + "=" + (num1 + num2));
+				break;
+			case '-':
+				System.out.println("" + num1 + op + num2 + "=" + (num1 - num2));
+				break;
+			case '*':
+				System.out.println("" + num1 + op + num2 + "=" + (num1 * num2));
+				break;
+			case '/':
+				System.out.println("" + num1 + op + num2 + "=" + ((double)num1 / num2));
+				break;
+			case '%':
+				System.out.println("" + num1 + op + num2 + "=" + (num1 % num2));
+				break;
+			default:
+				System.out.println("Not operator!");
+			}
+		}catch(InputMismatchException e) {
+			System.out.println("Wrong Input!");
+		}catch(ArithmeticException e) {
+			System.out.println("Not operation");
+		}catch(Exception e) {
+			System.out.println("Exception");	
+		}
+		System.out.println("EXIT!");
+		test1();
+		test2();
+		sc.close();
+
+	
+	}
+	public static void test1() {
+		try {
+			System.out.println(1/0);
+		}catch(Exception e) {
+			System.out.println("Exception!!!!!");
+		}finally {
+			System.out.println("Method EXIT!!");
+		}
+	}
+	public static void test2() {
+		try {
+			System.out.println(1/0);
+		}catch(Exception e) {
+			System.out.println("Exception!!!!!");
+			return;
+		}
+		System.out.println("Method2 EXIT!!");
+	}
+}
+
