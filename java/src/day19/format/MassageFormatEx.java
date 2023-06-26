49598001f58f4fe10f4884530271dd8e37bdb5ac
jeongjuseong <134986443+jeongjuseong@users.noreply.github.com>
2023-06-26 14:48:33 +0900

    자바 : format 예제

---
+++ java/src/day19/format/MassageFormatEx.java
@@ -0,0 +1,26 @@
+package day19.format;
+
+import java.text.MessageFormat;
+
+public class MassageFormatEx {
+
+	public static void main(String[] args) {
+		
+		int x = 1, y = 2;
+		String str = "(" + x + "," + y + ")";
+		System.out.println(str);
+		
+		x = 10;
+		y = 20;
+		String message = "({0},{1})";
+		String str2 = MessageFormat.format(message, x,y);
+		System.out.println(str2);
+		
+		x = 30;
+		y = 40;
+		str2 = MessageFormat.format(message, x,y);
+		System.out.println(str2);
+
+	}
+
+}
