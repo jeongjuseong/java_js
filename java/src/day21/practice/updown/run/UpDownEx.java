54a84e4b940f8ca642bf3ffb6f82576c3cce56dc
jeongjuseong <134986443+jeongjuseong@users.noreply.github.com>
2023-06-28 14:38:57 +0900

    자바 : updown 게임예제

---
+++ java/src/day21/practice/updown/run/UpDownEx.java
@@ -0,0 +1,16 @@
+package day21.practice.updown.run;
+
+import java.util.*;
+
+import day21.practice.updown.controller.GameManager;
+
+public class UpDownEx {
+
+	public static void main(String[] args) {
+		//UpDwon 프로그램을 작성하고, 기록도 관리하는 기능을 추가하세요.
+		GameManager gm = new GameManager();
+		gm.run();
+		
+	}
+
+}
